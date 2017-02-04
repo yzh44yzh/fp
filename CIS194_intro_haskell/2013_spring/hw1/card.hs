ce4c5eb7032a291bb1a9b3f2b5a5f9fc1d0d6c85
Yura Zhloba <yzh44yzh@gmail.com>
2017-02-04 22:08:53 +0300

    haskell implementation of hw1

---
+++ CIS194_intro_haskell/2013_spring/hw1/card.hs
@@ -0,0 +1,35 @@
+validate :: Int -> Bool
+validate cardNum =
+    let code = (sumDigits . dobleOddPositions . num2digits) cardNum in
+    code `rem` 10 == 0
+
+
+num2digits :: Int -> [Int]
+num2digits num =
+    num2digits' num []
+
+
+num2digits' :: Int -> [Int] -> [Int]
+num2digits' num acc =
+    if num < 10 then num : acc
+    else num2digits' d (r : acc)
+        where d = num `div` 10
+              r = num `rem` 10
+
+
+dobleOddPositions :: [Int] -> [Int]
+dobleOddPositions =
+    reverse . snd . foldl f a
+        where a = (True, [])
+              f = \acc val ->
+                  case acc of
+                    (True, lst) -> (False, (val * 2) : lst)
+                    (False, lst) -> (True, val : lst)
+
+
+sumDigits :: [Int] -> Int
+sumDigits =
+    foldl f 0
+        where f = \acc val ->
+                  if val > 10 then (sumDigits . num2digits) val + acc
+                  else val + acc
