07592e64eaea9851ae7aa7b45887699c6167a44a
Yuriy Zhloba <y_zhloba@wargaming.net>
2017-02-06 19:15:16 +0300

    done with hw1 scala implementation

--- CIS194_intro_haskell/2013_spring/hw1/card.scala
+++ CIS194_intro_haskell/2013_spring/hw1/card.scala
@@ -1,7 +1,40 @@
 object Card {
 
-  def num2digits(num: Int): List[Int] = {
-    List(1, num)
+  def validate(cardNum: Long): Boolean = {
+    val code = sumDigits(doubleOddPositions(num2digits(cardNum)))
+    code % 10 == 0
   }
 
+  def num2digits(num: Long): List[Int] = {
+    def _num2digits(num: Long): List[Int] = {
+      if (num < 10) List(num.toInt)
+      else {
+        val d = num / 10
+        val r = (num % 10).toInt
+        r :: _num2digits(d)
+      }
+    }
+    _num2digits(num).reverse
+  }
+
+  def doubleOddPositions(nums: List[Int]): List[Int] = {
+    def _even(n: List[Int]): List[Int] = n match {
+      case Nil => Nil
+      case h :: t => (h * 2) :: _odd(t)
+    }
+    def _odd(n: List[Int]): List[Int] = n match {
+      case Nil => Nil
+      case h :: t => h :: _even(t)
+    }
+    _even(nums)
+  }
+
+  def sumDigits(digits: List[Int]): Int = {
+    digits.foldLeft(0) {
+      (acc, d) => {
+        if(d < 10) d
+        else sumDigits(num2digits(d))
+      } + acc
+    }
+  }
 }
