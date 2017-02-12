90efef9ed00159ea33ed300b999a32b6b9c28458
Yura Zhloba <yzh44yzh@gmail.com>
2017-02-12 18:50:19 +0300

    hw1, hanoi ocaml

--- CIS194_intro_haskell/2013_spring/hw1/hanoi.ml
+++ CIS194_intro_haskell/2013_spring/hw1/hanoi.ml
@@ -7,6 +7,22 @@ exception Invalid_step of (pegName * pegName)
 exception Invalid_place of (peg * peg)
 
 
+let init (size : int) : state =
+  let rec _seq (next : int) : int list =
+    if next == size then [next]
+    else next :: _seq (next + 1)
+  in
+  if size < 1 then State ([], [], [], [])
+  else State (seq size, [], [], [])
+
+
+let place (fromPeg : peg) (toPeg : peg) : (peg * peg) =
+  match (fromPeg, toPeg) with
+  | (fhead :: ftail, []) -> (ftail, [fhead])
+  | (fhead :: ftail, thead :: _) when fhead < thead -> (ftail, fhead :: toPeg)
+  | _ -> raise (Invalid_place (fromPeg, toPeg))
+
+
 let step (fromPeg : pegName) (toPeg : pegName) (state : state) : state =
   let State (a, b, c, steps) = state in
   match (fromPeg, toPeg) with
@@ -19,8 +35,18 @@ let step (fromPeg : pegName) (toPeg : pegName) (state : state) : state =
   | _ -> raise (Invalid_step (fromPeg, toPeg))
 
 
-let place (fromPeg : peg) (toPeg : peg) : (peg * peg) =
-  match (fromPeg, toPeg) with
-  | (fhead :: ftail, []) -> (ftail, [fhead])
-  | (fhead :: ftail, thead :: _) when fhead < thead -> (ftail, fhead :: toPeg)
-  | _ -> raise (Invalid_place (fromPeg, toPeg))
+let rec move (numDisks : int) (fromPeg : pegName) (toPeg : pegName) (tempPeg : pegName) (state : state) : state =
+  match numDisks with
+  | 0 -> state
+  | 1 -> step fromPeg toPeg state
+  | 2 -> state |> step fromPeg tempPeg |> step fromPeg toPeg |> step tempPeg toPeg
+  | _ -> state
+         |> move (numDisks - 1) fromPeg tempPeg toPeg
+         |> step fromPeg toPeg
+         |> move (numDisks - 1) tempPeg toPeg fromPeg
+
+
+let solve (state : state) : state =
+  let State (a0, _, _, _) = state in
+  let State (a, b, c, steps) = move (List.length a0) A B C state in
+  State (a, b, c, List.rev steps)
